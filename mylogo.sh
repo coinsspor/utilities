133e1b90ad87390c03fda09dff1824363583795e
fatih erdal <38142283+coinsspor@users.noreply.github.com>
2024-04-20 14:03:20 +0300

    Update mylogo.sh

--- mylogo.sh
+++ mylogo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# This script prints "COINSSPOR" in colored ASCII art text using stars.
 
+
+
+echo " 𝐔𝐧𝐥𝐨𝐜𝐤 𝐭𝐡𝐞 𝐏𝐨𝐰𝐞𝐫 𝐨𝐟 𝐁𝐥𝐨𝐜𝐤𝐜𝐡𝐚𝐢𝐧: 𝐉𝐨𝐢𝐧 𝐭𝐡𝐞 𝐂𝐨𝐢𝐧𝐬𝐬𝐩𝐨𝐫 𝐍𝐨𝐝𝐞 𝐂𝐞𝐧𝐭𝐞𝐫 𝐓𝐨𝐝𝐚𝐲!"
 # Set color to blue
 echo -e "\033[0;34m"
 echo "░n░o░d░e░s░c░e░n░t░e░r░.░c░o░i░n░s░s░p░o░r░.░c░o░m░ "
