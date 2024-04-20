ee69c343ed3263dd59ec2ab360b54cd40d268a55
fatih erdal <38142283+coinsspor@users.noreply.github.com>
2024-04-20 13:52:13 +0300

    Update mylogo.sh

--- mylogo.sh
+++ mylogo.sh
@@ -3,16 +3,12 @@
 
 # Set color to blue
 echo -e "\033[0;34m"
-echo " ******   ***   *****   *** ******   ***    *** ****** ****    *** "
-echo "**    ** ** ** **   ** ** **     ** ** ** ** **    ** ** **  ** **"
-echo "**      **   ** *****  ** **     ** **  **** **    ** **  ****  **"
-echo "**  *** ******* **     ** **     ** **   **  **    ** **   **   **"
-echo "**    ** **   ** **     ** **     ** **        **  ** **        **"
-echo " ******  **   ** **     **  ******  **        ** ******        ** "
+echo "░n░o░d░e░s░c░e░n░t░e░r░.░c░o░i░n░s░s░p░o░r░.░c░o░m░ "
+
 
 # Set color to green
 echo -e "\033[0;32m"
-echo "                                                                       nodescenter.coinsspor.com"
+echo "           nodescenter.coinsspor.com"
 
 # Reset color to default
 echo -e "\033[0m"
